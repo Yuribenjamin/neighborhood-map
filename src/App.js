04ce4c025ea1230ac71557bca578e5264940d00b
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-27 16:06:39 +0200

    init event click to display info

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     this.renderMap()
     this.getVenues()
   }
+
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB2Uc0ijnu0YBte1XEnPcChy6CRn84VTNE&callback=initMap")
     window.initMap = this.initMap
@@ -37,20 +38,31 @@ class App extends Component {
         console.log("ERROR!! " + error)
       })
   }
+  
 
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 30.0444, lng: 31.2357},
       zoom: 13
     })
+
+    var infowindow = new window.google.maps.InfoWindow()
+
     this.state.venues.map(myVenue => {
+      var contentString = `${myVenue.venue.name}`
       var marker = new window.google.maps.Marker({
         position: {lat: myVenue.venue.location.lat , lng: myVenue.venue.location.lng},
         map: map,
         title: myVenue.venue.name
       })
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString)
+        infowindow.open(map, marker)
+      })
+
     })
   }
+
 
   render() {
     return (
@@ -60,6 +72,7 @@ class App extends Component {
     );
   }
 }
+
 
 function loadScript (url) {
   var index  = window.document.getElementsByTagName("script")[0]
