a63cd6759ac3b7a0920caaa7aacb6cf47e5f080c
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-28 16:35:55 +0200

    Add comment & Add authFailure if data requests fail

--- src/Mymap.js
+++ src/Mymap.js
@@ -14,11 +14,20 @@ class Mymap extends Component {
         this.getVenues()
       }
     
+      // function that render map and display alret if map failed to load
+
       renderMap = () => {
         loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB2Uc0ijnu0YBte1XEnPcChy6CRn84VTNE&callback=initMap")
         window.initMap = this.initMap
+        // if map failed to load 
+        //https://developers.google.com/maps/documentation/javascript/events
+        window.gm_authFailure = function() {
+            alert('Google maps failed to load!')
+      } 
       }
     
+      // function that fetch veneues using axios package
+
       getVenues = () => {
         const endPoint = "https://api.foursquare.com/v2/venues/explore?"
         const parameters = {
@@ -39,25 +48,29 @@ class Mymap extends Component {
           })
       }
       
-    
+      // function that initialize the map
       initMap = () => {
         var map = new window.google.maps.Map(document.getElementById('map'), {
           center: {lat: 30.0444, lng: 31.2357},
           zoom: 13
         })
-    
+        // ceate information window 
         var infowindow = new window.google.maps.InfoWindow()
-    
+        // loop throw each venue use foreach to display dynamic markers
         this.state.venues.forEach(myVenue => {
           var contentString = `${myVenue.venue.name}`
+          // create the marker
           var marker = new window.google.maps.Marker({
             position: {lat: myVenue.venue.location.lat,
             lng: myVenue.venue.location.lng},
             map: map,
             title: myVenue.venue.name
           })
+          // event handle click on the marker
           marker.addListener('click', function() {
+            // change the contet
             infowindow.setContent(contentString)
+            // and open the info window
             infowindow.open(map, marker)
           })
     
