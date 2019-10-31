d8d1ee8f2ff462a3e0729bd45ae824c46486fe4c
Ibrahim Ragab <Abrahammoustafa@hotmail.co.uk>
2019-10-31 13:25:25 +0200

    fix minor for build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.register();
