8b662a2f3dea0d71d76403f57589576837ef4d8a
Ibrahim Ragab <Abrahammoustafa@hotmail.co.uk>
2019-10-31 13:17:53 +0200

    Updates

---
+++ precache-manifest.5c6646566bff033f033396b2fa0f478e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab5f7e8aac9afc49f6431a59de50201",
+    "url": "/neighborhood-map/index.html"
+  },
+  {
+    "revision": "88089c7d8d0722661c30",
+    "url": "/neighborhood-map/static/css/main.c598e6b1.chunk.css"
+  },
+  {
+    "revision": "86ca915e9ede1d6fe0a6",
+    "url": "/neighborhood-map/static/js/2.6dcb5db4.chunk.js"
+  },
+  {
+    "revision": "88089c7d8d0722661c30",
+    "url": "/neighborhood-map/static/js/main.9ef7c290.chunk.js"
+  },
+  {
+    "revision": "6bd0e54fd983034bc260",
+    "url": "/neighborhood-map/static/js/runtime-main.839a2cbf.js"
+  }
+]);
